457413fdd6cfd615b7ae62c6f5f43b4a3d209bb7
Manisha Choudhary <manishachoudhary9028@gmail.com>
2026-02-09 22:55:45 +0530

    Day21 - min number in array

---
+++ searching/min.java
@@ -0,0 +1,21 @@
+package searching;
+public class min {
+    
+class GfG {
+    static int findMin(int[] arr) {
+        int res = arr[0];
+
+        // Traverse over arr[] to find minimum element
+        for (int i = 1; i < arr.length; i++) {
+            res = Math.min(res, arr[i]);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 6, 1, 2, 3, 4};
+        System.out.println(findMin(arr));
+    }
+}
+}
